e366c9c7c9f103d368afd5e2b1aa7d00ca63d235
camilogs <camilo.gonzalez.col@gmail.com>
2023-12-16 08:19:15 -0500

    Simplify new expression

--- AutoCallsApi/Program.cs
+++ AutoCallsApi/Program.cs
@@ -4,7 +4,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        Startup startup = new Startup(builder.Configuration);
+        Startup startup = new(builder.Configuration);
 
         startup.ConfigureServices(builder.Services);
 
